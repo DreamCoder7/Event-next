812709e0ca96abee0cadd686afbf31892d21f270
Abraham <nigussieabreham80@gmail.com>
2023-01-23 22:08:03 +0300

    Adding Wrapper layout and event search for filtering the event

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,11 +1,24 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+import { getAllEvents } from "dummy-data";
+import EventList from "@/components/EventList/event-list";
+import EventSearch from "@/components/EventList/event-search";
 
 function EventPage() {
+  const router = useRouter();
+  const events = getAllEvents();
 
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
 
   return (
-    <div>
-      <h1>Event Page</h1>
-    </div>
+    <Fragment>
+      <EventSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
   );
 }
 
